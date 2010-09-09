40c55356539a5a8b4865db365f87f3a7add540e7
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-09 12:46:17 +0600

    Add factory method for creating content type id
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- content-api/src/main/java/com/smartitengineering/cms/api/type/ContentTypeLoader.java
+++ content-api/src/main/java/com/smartitengineering/cms/api/type/ContentTypeLoader.java
@@ -18,8 +18,9 @@
  */
 package com.smartitengineering.cms.api.type;
 
-import java.io.File;
+import com.smartitengineering.cms.api.WorkspaceId;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collection;
 
 /**
@@ -30,97 +31,98 @@ import java.util.Collection;
  */
 public interface ContentTypeLoader {
 
-		/**
-		 * Loads the content type definition specified by the content type ID.
-		 * @param contentTypeID The id of content type to retrieve.
-		 * @return Content type represented by the parameter. Null if no such type
-		 *					exists.
-		 * @throws NullPointerException If contentTypeID parameter is null
-		 */
-		public ContentType loadContentType(ContentTypeId contentTypeID)
-						throws NullPointerException;
+  /**
+   * Loads the content type definition specified by the content type ID.
+   * @param contentTypeID The id of content type to retrieve.
+   * @return Content type represented by the parameter. Null if no such type
+   *					exists.
+   * @throws NullPointerException If contentTypeID parameter is null
+   */
+  public ContentType loadContentType(ContentTypeId contentTypeID)
+      throws NullPointerException;
 
-		/**
-		 * Parse a {@link File} to retrieve the content type definitions. The format
-		 * of the file should be specified by the implementor.
-		 * @param contentTypeDefinition File to parse for content type definitions.
-		 * @return Return ordered collection of parsed content types. Could be empty
-		 *					but never null.
-		 * @throws NullPointerException If contentTypeDefinition is null
-		 * @throws IOException If there is any error during parsing.
-		 */
-		public Collection<MutableContentType> parseContentTypes(
-						File contentTypeDefinition)
-						throws NullPointerException,
-									 IOException;
+  /**
+   * Parse a {@link InputStream} to retrieve the content type definitions. The format
+   * of the file should be specified by the implementor.
+   * @param contentTypeDefinitionStream Stream to parse for content type definitions.
+   * @return Return ordered collection of parsed content types. Could be empty
+   *					but never null.
+   * @throws NullPointerException If contentTypeDefinition is null
+   * @throws IOException If there is any error during parsing.
+   */
+  public Collection<MutableContentType> parseContentTypes(
+      InputStream contentTypeDefinitionStream)
+      throws NullPointerException,
+             IOException;
 
-		/**
-		 * Get the mutable version of the content data type.
-		 * @param contentDataType Content data type to get the mutable version for
-		 * @return Editable content data type
-		 */
-		public MutableContentDataType getMutableContentDataType(
-						ContentDataType contentDataType);
+  /**
+   * Get the mutable version of the content data type.
+   * @param contentDataType Content data type to get the mutable version for
+   * @return Editable content data type
+   */
+  public MutableContentDataType getMutableContentDataType(
+      ContentDataType contentDataType);
 
-		/**
-		 * Get the mutable version of the collection data type.
-		 * @param collectionDataType Collection data type to get the mutable one for
-		 * @return Editable collection data type
-		 */
-		public MutableCollectionDataType getMutableCollectionDataType(
-						CollectionDataType collectionDataType);
+  /**
+   * Get the mutable version of the collection data type.
+   * @param collectionDataType Collection data type to get the mutable one for
+   * @return Editable collection data type
+   */
+  public MutableCollectionDataType getMutableCollectionDataType(
+      CollectionDataType collectionDataType);
 
-		/**
-		 * Get the mutable version of the content status
-		 * @param contentStatus Content status to get the mutable version for
-		 * @return Editable content status
-		 */
-		public MutableContentStatus getMutableContentStatus(
-						ContentStatus contentStatus);
+  /**
+   * Get the mutable version of the content status
+   * @param contentStatus Content status to get the mutable version for
+   * @return Editable content status
+   */
+  public MutableContentStatus getMutableContentStatus(
+      ContentStatus contentStatus);
 
-		/**
-		 * Get the mutable version of the content type ID
-		 * @param contentTypeID content type id to get the mutable version for
-		 * @return Editable content type id
-		 */
-		public MutableContentTypeId getMutableContentTypeID(
-						ContentTypeId contentTypeID);
+  /**
+   * Get the mutable version of the content type ID
+   * @param contentTypeID content type id to get the mutable version for
+   * @return Editable content type id
+   */
+  public MutableContentTypeId getMutableContentTypeID(
+      ContentTypeId contentTypeID);
 
-		/**
-		 * Get the mutable version of the field definition
-		 * @param fieldDef Field definition to get the mutable version for
-		 * @return Editable field definition
-		 */
-		public MutableFieldDef getMutableFieldDef(FieldDef fieldDef);
+  /**
+   * Get the mutable version of the field definition
+   * @param fieldDef Field definition to get the mutable version for
+   * @return Editable field definition
+   */
+  public MutableFieldDef getMutableFieldDef(FieldDef fieldDef);
 
-		/**
-		 * Create a new editable collection data type
-		 * @return New collection data type
-		 */
-		public MutableCollectionDataType createMutableCollectionDataType();
+  /**
+   * Create a new editable collection data type
+   * @return New collection data type
+   */
+  public MutableCollectionDataType createMutableCollectionDataType();
 
-		/**
-		 * Create a new editable content data type
-		 * @return New content data type
-		 */
-		public MutableContentDataType createMutableContentDataType();
+  /**
+   * Create a new editable content data type
+   * @return New content data type
+   */
+  public MutableContentDataType createMutableContentDataType();
 
-		/**
-		 * Create a new editable content status
-		 * @return
-		 */
-		public MutableContentStatus createMutableContentStatus();
+  /**
+   * Create a new editable content status
+   * @return
+   */
+  public MutableContentStatus createMutableContentStatus();
 
-		/**
-		 * Create a new editable content type id.
-		 * @return New content type id
-		 */
-		public MutableContentTypeId createMutableContentTypeID();
+  /**
+   * Create a new editable content type id.
+   * @return New content type id
+   */
+  public MutableContentTypeId createMutableContentTypeID();
 
-		/**
-		 * Create a new editable field definition
-		 * @return New field definition
-		 */
-		public MutableFieldDef createMutableFieldDef();
+  /**
+   * Create a new editable field definition
+   * @return New field definition
+   */
+  public MutableFieldDef createMutableFieldDef();
 
+  public ContentTypeId createContentTypeId(WorkspaceId workspaceId, String namespace, String name);
 }
