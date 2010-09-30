f2477d4253ad2673ae21ea90ebec11f70e1f7337
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-09-30 20:26:23 +0600

    Avoid using Jackson JAXB serializer for JSON serialization
    This is mainly to ensure not getting into JAXB hell. Just extended Jackson
    class as a blank and avoided adding all 4 providers of Jackson JAX-RS.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ webservice/src/main/java/com/smartitengineering/cms/ws/JacksonJsonProvider.java
@@ -0,0 +1,34 @@
+/*
+ *
+ * This is a simple Content Management System (CMS)
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.cms.ws;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author imyousuf
+ */
+@Provider
+@Consumes({MediaType.APPLICATION_JSON, "text/json"})
+@Produces({MediaType.APPLICATION_JSON, "text/json"})
+public class JacksonJsonProvider extends org.codehaus.jackson.jaxrs.JacksonJsonProvider {
+}
