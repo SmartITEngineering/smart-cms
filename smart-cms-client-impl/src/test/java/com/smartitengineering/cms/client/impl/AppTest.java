464079603c4c67e57f492e58c0c4d57629cf4abc
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-01 18:31:42 +0600

    Add test case to verify root resource activities
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-cms-client-impl/src/test/java/com/smartitengineering/cms/client/impl/AppTest.java
+++ smart-cms-client-impl/src/test/java/com/smartitengineering/cms/client/impl/AppTest.java
@@ -3,11 +3,25 @@ package com.smartitengineering.cms.client.impl;
 import com.google.inject.AbstractModule;
 import com.smartitengineering.cms.binder.guice.Initializer;
 import com.smartitengineering.cms.client.api.RootResource;
+import com.smartitengineering.cms.client.api.WorkspaceContentResouce;
+import com.smartitengineering.cms.client.api.domains.Workspace;
 import com.smartitengineering.util.bean.guice.GuiceUtil;
+import com.smartitengineering.util.rest.client.ApplicationWideClientFactoryImpl;
+import com.smartitengineering.util.rest.client.ClientUtil;
+import com.smartitengineering.util.rest.client.ResourceLink;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.Properties;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
 import org.apache.hadoop.hbase.HColumnDescriptor;
@@ -27,10 +41,14 @@ import org.slf4j.LoggerFactory;
  */
 public class AppTest {
 
+  private static final int PORT = 10080;
+  public static final String DEFAULT_NS = "com.smartitengineering";
+  public static final String ROOT_URI_STRING = "http://localhost:" + PORT + "/";
+  public static final String TEST = "test";
+  public static final String TEST_NS = "testNS";
   private static final HBaseTestingUtility TEST_UTIL = new HBaseTestingUtility();
   private static final Logger LOGGER = LoggerFactory.getLogger(AppTest.class);
   private static Server jettyServer;
-  private static final int PORT = 10080;
 
   @BeforeClass
   public static void globalSetup() throws Exception {
@@ -73,7 +91,13 @@ public class AppTest {
     }
     WebAppContext webAppHandler = new WebAppContext(webapp, "/");
     jettyServer.setHandler(webAppHandler);
+    jettyServer.setSendDateHeader(true);
     jettyServer.start();
+
+    /*
+     * Setup client properties
+     */
+    System.setProperty(ApplicationWideClientFactoryImpl.TRACE, "true");
   }
 
   @AfterClass
@@ -83,10 +107,68 @@ public class AppTest {
   }
 
   @Test
-  public void testApp() throws URISyntaxException {
+  public void testStartup() throws URISyntaxException {
     RootResource resource = RootResourceImpl.getRoot(new URI("http://localhost:" + PORT + "/"));
     Assert.assertNotNull(resource);
     Assert.assertEquals(0, resource.getWorkspaces().size());
+  }
+
+  @Test
+  public void testCreationAndRetrievalWithNameOnly() throws URISyntaxException {
+    RootResource resource = RootResourceImpl.getRoot(new URI("http://localhost:" + PORT + "/"));
+    final MultivaluedMap<String, String> map = new MultivaluedMapImpl();
+    map.add("name", TEST);
+    ClientResponse response = resource.post(MediaType.APPLICATION_FORM_URLENCODED, map, ClientResponse.Status.CREATED);
+    ResourceLink link = ClientUtil.createResourceLink(WorkspaceContentResouce.WORKSPACE_CONTENT, response.getLocation(),
+                                                      MediaType.APPLICATION_JSON);
+    WorkspaceContentResouce workspaceContentResource = new WorkspaceContentResourceImpl(resource, link);
+    Assert.assertNotNull(workspaceContentResource.getLastReadStateOfEntity());
+    Workspace workspace = workspaceContentResource.getLastReadStateOfEntity();
+    Assert.assertEquals(TEST, workspace.getId().getName());
+    Assert.assertEquals(DEFAULT_NS, workspace.getId().getGlobalNamespace());
+    Collection<WorkspaceContentResouce> resources = resource.getWorkspaces();
+    Assert.assertEquals(1, resource.getWorkspaces().size());
+    workspaceContentResource = resources.iterator().next();
+    workspace = workspaceContentResource.getLastReadStateOfEntity();
+    Assert.assertEquals(TEST, workspace.getId().getName());
+    Assert.assertEquals(DEFAULT_NS, workspace.getId().getGlobalNamespace());
+  }
+
+  @Test
+  public void testCreationAndRetrievalWithNamespace() throws URISyntaxException {
+    RootResource resource = RootResourceImpl.getRoot(new URI(ROOT_URI_STRING));
+    final MultivaluedMap<String, String> map = new MultivaluedMapImpl();
+    map.add("name", TEST);
+    map.add("namespace", TEST_NS);
+    ClientResponse response = resource.post(MediaType.APPLICATION_FORM_URLENCODED, map, ClientResponse.Status.CREATED);
+    ResourceLink link = ClientUtil.createResourceLink(WorkspaceContentResouce.WORKSPACE_CONTENT, response.getLocation(),
+                                                      MediaType.APPLICATION_JSON);
+    WorkspaceContentResouce workspaceContentResource = new WorkspaceContentResourceImpl(resource, link);
+    Assert.assertNotNull(workspaceContentResource.getLastReadStateOfEntity());
+    Workspace workspace = workspaceContentResource.getLastReadStateOfEntity();
+    Assert.assertEquals(TEST, workspace.getId().getName());
+    Assert.assertEquals(TEST_NS, workspace.getId().getGlobalNamespace());
+    Collection<WorkspaceContentResouce> resources = resource.getWorkspaces();
+    Assert.assertEquals(2, resource.getWorkspaces().size());
+    workspaceContentResource = resources.iterator().next();
+    workspace = workspaceContentResource.getLastReadStateOfEntity();
+    Assert.assertEquals(TEST, workspace.getId().getName());
+    Assert.assertEquals(TEST_NS, workspace.getId().getGlobalNamespace());
+  }
+
+  @Test
+  public void testConditionalRootResourceGet() throws Exception {
+    HttpClient client = new HttpClient();
+    GetMethod method = new GetMethod(ROOT_URI_STRING);
+    client.executeMethod(method);
+    Assert.assertEquals(200, method.getStatusCode());
+    Header date = method.getResponseHeader(HttpHeaders.LAST_MODIFIED);
+    String dateStr = date.getValue();
+    Header ifDate = new Header(HttpHeaders.IF_MODIFIED_SINCE, dateStr);
+    method = new GetMethod(ROOT_URI_STRING);
+    method.addRequestHeader(ifDate);
+    client.executeMethod(method);
+    Assert.assertEquals(304, method.getStatusCode());
   }
 
   public static class ConfigurationModule extends AbstractModule {
