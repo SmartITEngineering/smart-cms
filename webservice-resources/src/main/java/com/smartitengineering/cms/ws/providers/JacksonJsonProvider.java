43058c74efefa91c7caef01d962c4d4e2c20bd55
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-02 10:24:17 +0600

    Create a new package for providers and refactor current ones there
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- webservice-resources/src/main/java/com/smartitengineering/cms/ws/providers/JacksonJsonProvider.java
+++ webservice-resources/src/main/java/com/smartitengineering/cms/ws/providers/JacksonJsonProvider.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.smartitengineering.cms.ws;
+package com.smartitengineering.cms.ws.providers;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
