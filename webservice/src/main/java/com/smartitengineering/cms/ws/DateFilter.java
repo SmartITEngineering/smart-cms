ab9b1c4779b4f2e36122c538d975afbfad05499a
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-10-02 10:20:27 +0600

    Add DateFilter to ensure that we do not depend on container for dates
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ webservice/src/main/java/com/smartitengineering/cms/ws/DateFilter.java
@@ -0,0 +1,57 @@
+/*
+ *
+ * This is a simple Content Management System (CMS)
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.cms.ws;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.HttpHeaders;
+
+/**
+ *
+ * @author imyousuf@smartitengineering.com
+ */
+public class DateFilter implements Filter {
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+  }
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+                                                                                                   ServletException {
+    if (!(request instanceof HttpServletRequest)) {
+      throw new ServletException("Not a HTTP container");
+    }
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    chain.doFilter(request, response);
+    httpResponse.setDateHeader(HttpHeaders.DATE, new Date().getTime());
+  }
+
+  @Override
+  public void destroy() {
+  }
+}
