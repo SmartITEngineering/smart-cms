bec1ec3eae27500261ddaf6f506159be1750bc37
kaisar <kaisar@smartitengineering.com>
2010-10-17 14:55:59 +0600

    Add APIs to for container Resource.

---
+++ smart-cms-client-api/src/main/java/com/smartitengineering/cms/client/api/ContainerResource.java
@@ -0,0 +1,38 @@
+/*
+ *
+ * This is a simple Content Management System (CMS)
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.cms.client.api;
+
+import com.smartitengineering.cms.ws.common.domains.Content;
+import com.smartitengineering.util.rest.client.WritableResource;
+import java.net.URI;
+import java.util.Collection;
+import org.apache.abdera.model.Feed;
+
+/**
+ *
+ * @author kaisar
+ */
+public interface ContainerResource extends WritableResource<Feed> {
+
+  public void createContainer(URI contentUri);
+
+  public void updateContainer(URI contentUri);
+
+  public Collection<ContentResource> getContainerContents();
+}
