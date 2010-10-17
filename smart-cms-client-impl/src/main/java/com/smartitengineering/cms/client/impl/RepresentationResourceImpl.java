827a048d028403bce937acaa4d9ab99ccf261919
kaisar <kaisar@smartitengineering.com>
2010-10-17 18:53:37 +0600

    Add APIs and Implementations for Representations and Variations.
    The implementation is not completed.
    
    Signed-off-by: kaisar <kaisar@smartitengineering.com>

---
+++ smart-cms-client-impl/src/main/java/com/smartitengineering/cms/client/impl/RepresentationResourceImpl.java
@@ -0,0 +1,61 @@
+/*
+ *
+ * This is a simple Content Management System (CMS)
+ * Copyright (C) 2010  Imran M Yousuf (imyousuf@smartitengineering.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.cms.client.impl;
+
+import com.smartitengineering.cms.client.api.RepresentationResource;
+import com.smartitengineering.util.rest.client.AbstractClientResource;
+import com.smartitengineering.util.rest.client.Resource;
+import com.smartitengineering.util.rest.client.ResourceLink;
+import com.sun.jersey.api.client.config.ClientConfig;
+
+/**
+ *
+ * @author kaisar
+ */
+public class RepresentationResourceImpl extends AbstractClientResource<String, Resource> implements
+    RepresentationResource {
+
+  public RepresentationResourceImpl(Resource referrer, ResourceLink link) {
+    super(referrer, link);
+  }
+
+  @Override
+  protected void processClientConfig(ClientConfig clientConfig) {
+  }
+
+  @Override
+  protected ResourceLink getNextUri() {
+    return null;
+  }
+
+  @Override
+  protected ResourceLink getPreviousUri() {
+    return null;
+  }
+
+  @Override
+  protected Resource instantiatePageableResource(ResourceLink link) {
+    return null;
+  }
+
+  @Override
+  public String getRepresentation(String representationName) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+}
