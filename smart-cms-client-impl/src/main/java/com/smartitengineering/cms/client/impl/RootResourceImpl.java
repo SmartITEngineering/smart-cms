9bb6ac790b676e8248428b820e9387d023ababb0
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-04-18 16:42:55 +0600

    Use configurable connection for cms client impl
    Name of the properties file for CMS Client config is -
    smart-cms-client-config.properties It is not bundled by default so apps
    using the client impl may bundle a .template config.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- smart-cms-client-impl/src/main/java/com/smartitengineering/cms/client/impl/RootResourceImpl.java
+++ smart-cms-client-impl/src/main/java/com/smartitengineering/cms/client/impl/RootResourceImpl.java
@@ -27,9 +27,13 @@ import com.smartitengineering.cms.ws.common.domains.Workspace;
 import com.smartitengineering.cms.ws.common.domains.WorkspaceId;
 import com.smartitengineering.cms.ws.common.providers.JacksonJsonProvider;
 import com.smartitengineering.cms.ws.common.providers.TextURIListProvider;
+import com.smartitengineering.util.bean.PropertiesLocator;
 import com.smartitengineering.util.rest.atom.AbstractFeedClientResource;
 import com.smartitengineering.util.rest.atom.AtomClientUtil;
+import com.smartitengineering.util.rest.client.ApplicationWideClientFactoryImpl;
 import com.smartitengineering.util.rest.client.ClientUtil;
+import com.smartitengineering.util.rest.client.ConfigProcessor;
+import com.smartitengineering.util.rest.client.ConnectionConfig;
 import com.smartitengineering.util.rest.client.Resource;
 import com.smartitengineering.util.rest.client.ResourceLink;
 import com.sun.jersey.api.client.ClientResponse;
@@ -37,18 +41,22 @@ import com.sun.jersey.api.client.UniformInterfaceException;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.atom.abdera.impl.provider.entity.FeedProvider;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
 import org.apache.abdera.model.Entry;
 import org.apache.abdera.model.Feed;
 import org.apache.abdera.model.Link;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -62,10 +70,44 @@ public class RootResourceImpl extends AbstractFeedClientResource<Resource<? exte
   private static final int PORT = 10080;
   public static final String ROOT_URI_STRING = "http://localhost:" + PORT + "/";
   public static final String REL_TEMPLATES = "templates";
+  private static final ConnectionConfig SMART_CMS_CONNECTION_CONFIG;
+  private static final boolean CONNECTION_CONFIGURED;
+  private static final URI SMART_CMS_BASE_URI;
+  private static final ConfigProcessor CONFIG_PROCESSOR = new CmsConfigProcessor();
+
+  static {
+    SMART_CMS_CONNECTION_CONFIG = new ConnectionConfig();
+    String propFileName = "smart-cms-client-config.properties";
+    PropertiesLocator locator = new PropertiesLocator();
+    locator.setSmartLocations(propFileName);
+    final Properties properties = new Properties();
+    try {
+      locator.loadProperties(properties);
+    }
+    catch (IOException ex) {
+      LOGGER.warn("Exception!", ex);
+    }
+    if (!properties.isEmpty()) {
+      CONNECTION_CONFIGURED = true;
+      SMART_CMS_CONNECTION_CONFIG.setBasicUri(properties.getProperty("baseUri", ""));
+      SMART_CMS_CONNECTION_CONFIG.setContextPath(properties.getProperty("contextPath", "/"));
+      SMART_CMS_CONNECTION_CONFIG.setHost(properties.getProperty("host", "localhost"));
+      SMART_CMS_CONNECTION_CONFIG.setPort(NumberUtils.toInt(properties.getProperty("port", ""), 9090));
+      SMART_CMS_BASE_URI = UriBuilder.fromUri(SMART_CMS_CONNECTION_CONFIG.getContextPath()).path(SMART_CMS_CONNECTION_CONFIG.
+          getBasicUri()).host(SMART_CMS_CONNECTION_CONFIG.getHost()).port(SMART_CMS_CONNECTION_CONFIG.getPort()).
+          scheme("http").build();
+    }
+    else {
+      CONNECTION_CONFIGURED = false;
+      SMART_CMS_BASE_URI = null;
+    }
+  }
 
   private RootResourceImpl(URI uri) throws IllegalArgumentException,
                                            UniformInterfaceException {
-    super(null, uri, false, null);
+    super(null, CONNECTION_CONFIGURED && uri == null ? SMART_CMS_BASE_URI : uri, false,
+          CONNECTION_CONFIGURED ? ApplicationWideClientFactoryImpl.getClientFactory(SMART_CMS_CONNECTION_CONFIG,
+                                                                                    CONFIG_PROCESSOR) : null);
     if (logger.isDebugEnabled()) {
       logger.debug("Root resource URI for Smart CMS " + uri.toString());
     }
@@ -73,9 +115,20 @@ public class RootResourceImpl extends AbstractFeedClientResource<Resource<? exte
 
   @Override
   protected void processClientConfig(ClientConfig clientConfig) {
-    clientConfig.getClasses().add(JacksonJsonProvider.class);
-    clientConfig.getClasses().add(TextURIListProvider.class);
-    clientConfig.getClasses().add(FeedProvider.class);
+    CONFIG_PROCESSOR.process(clientConfig);
+  }
+
+  private static class CmsConfigProcessor implements ConfigProcessor {
+
+    public CmsConfigProcessor() {
+    }
+
+    @Override
+    public void process(ClientConfig clientConfig) {
+      clientConfig.getClasses().add(JacksonJsonProvider.class);
+      clientConfig.getClasses().add(TextURIListProvider.class);
+      clientConfig.getClasses().add(FeedProvider.class);
+    }
   }
 
   @Override
