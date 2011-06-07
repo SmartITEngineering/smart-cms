1ebd1fd192af71f00c63edc1ddea687b0631b357
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-06-07 12:11:37 +0600

    Add the dao impl for persisting in the content repository
    The implementation depends on a converter adapter which converts from
    bean to content and vice versa. This way only a converter helper needs to
    be implemented from the content type definitions.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ code-generation/content-repo-dao-impl/src/main/java/com/smartitengineering/cms/repo/dao/impl/AbstractRepoAdapterHelper.java
@@ -0,0 +1,131 @@
+package com.smartitengineering.cms.repo.dao.impl;
+
+import com.google.inject.Inject;
+import com.smartitengineering.cms.api.content.Content;
+import com.smartitengineering.cms.api.content.ContentId;
+import com.smartitengineering.cms.api.factory.SmartContentAPI;
+import com.smartitengineering.cms.api.factory.content.ContentLoader;
+import com.smartitengineering.cms.api.factory.content.WriteableContent;
+import com.smartitengineering.cms.api.type.ContentStatus;
+import com.smartitengineering.cms.api.type.ContentType;
+import com.smartitengineering.cms.api.type.ContentTypeId;
+import com.smartitengineering.cms.api.workspace.WorkspaceId;
+import com.smartitengineering.util.bean.adapter.AbstractAdapterHelper;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author imyousuf
+ */
+public abstract class AbstractRepoAdapterHelper<T extends AbstractRepositoryDomain<T>> extends AbstractAdapterHelper<Content, T> {
+
+  protected transient final Logger logger = LoggerFactory.getLogger(getClass());
+  protected final Class<? extends T> beanClass;
+  private ContentTypeId contentTypeId;
+  @Inject
+  private WorkspaceId defaultContainerWorkspace;
+
+  protected AbstractRepoAdapterHelper() {
+    beanClass = initializeEntityClassFromGenerics();
+  }
+
+  protected AbstractRepoAdapterHelper(Class<? extends T> entityClass) {
+    beanClass = entityClass;
+  }
+
+  protected ContentTypeId getContentTypeId() {
+    if (contentTypeId == null) {
+      String typeNS = beanClass.getPackage().getName();
+      String typeName = beanClass.getSimpleName();
+      contentTypeId = SmartContentAPI.getInstance().getContentTypeLoader().createContentTypeId(defaultContainerWorkspace,
+                                                                                               typeNS, typeName);
+    }
+    return contentTypeId;
+  }
+
+  protected final Class<? extends T> initializeEntityClassFromGenerics() {
+    Class<? extends T> extractedEntityClass = null;
+    try {
+      Type paramType =
+           ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+      if (paramType instanceof ParameterizedType) {
+        paramType = ((ParameterizedType) paramType).getRawType();
+      }
+      Class<T> pesistenceRegistryClass = paramType instanceof Class ? (Class<T>) paramType : null;
+      if (logger.isDebugEnabled()) {
+        logger.debug("Entity class predicted to: " + pesistenceRegistryClass.toString());
+      }
+      extractedEntityClass = pesistenceRegistryClass;
+    }
+    catch (Exception ex) {
+      logger.warn("Could not predict entity class ", ex);
+    }
+    return extractedEntityClass;
+  }
+
+  @Override
+  protected void mergeFromF2T(Content fromBean, T toBean) {
+    if (fromBean.getStatus() != null) {
+      toBean.setStatus(fromBean.getStatus().getName());
+    }
+    if (fromBean.getContentId() != null) {
+      toBean.setId(org.apache.commons.codec.binary.StringUtils.newStringUtf8(fromBean.getContentId().getId()));
+      toBean.setWorkspaceId(fromBean.getContentId().getWorkspaceId().toString());
+    }
+    toBean.setCreationDate(fromBean.getCreationDate());
+    toBean.setLastModificationDate(fromBean.getLastModifiedDate());
+    toBean.setEntityValue(fromBean.getEntityTagValue());
+    mergeContentIntoBean(fromBean, toBean);
+  }
+
+  @Override
+  protected Content convertFromT2F(T toBean) {
+    final SmartContentAPI instance = SmartContentAPI.getInstance();
+    final ContentLoader contentLoader = instance.getContentLoader();
+    WriteableContent content = contentLoader.createContent(getContentTypeId().getContentType());
+    if (StringUtils.isNotBlank(toBean.getWorkspaceId()) && StringUtils.isNotBlank(toBean.getId())) {
+      String[] idParts = toBean.getWorkspaceId().split(":");
+      if (idParts == null || idParts.length != 2) {
+        throw new IllegalArgumentException("Workspace ID must be set as 'namespace:name'");
+      }
+      final WorkspaceId wId = instance.getWorkspaceApi().createWorkspaceId(idParts[0], idParts[1]);
+      final byte[] bytesUtf8 = org.apache.commons.codec.binary.StringUtils.getBytesUtf8(toBean.getId());
+      ContentId cId = contentLoader.createContentId(wId, bytesUtf8);
+      content.setContentId(cId);
+    }
+    else {
+      final WorkspaceId wId;
+      if (StringUtils.isNotBlank(toBean.getWorkspaceId())) {
+        String[] idParts = toBean.getWorkspaceId().split(":");
+        if (idParts == null || idParts.length != 2) {
+          throw new IllegalArgumentException("Workspace ID must be set as 'namespace:name'");
+        }
+        wId = instance.getWorkspaceApi().createWorkspaceId(idParts[0], idParts[1]);
+      }
+      else {
+        wId = defaultContainerWorkspace;
+      }
+      content.createContentId(wId);
+    }
+    final ContentStatus status;
+    final ContentType type = getContentTypeId().getContentType();
+    if (StringUtils.isNotBlank(toBean.getStatus())) {
+
+      status = type.getStatuses().get(toBean.getStatus());
+    }
+    else {
+      status = type.getStatuses().entrySet().iterator().next().getValue();
+    }
+    content.setStatus(status);
+    mergeBeanIntoContent(toBean, content);
+    return content;
+  }
+
+  protected abstract void mergeContentIntoBean(Content fromBean, T toBean);
+
+  protected abstract void mergeBeanIntoContent(T fromBean, WriteableContent toBean);
+}
---
+++ code-generation/content-repo-dao-impl/src/main/java/com/smartitengineering/cms/repo/dao/impl/RepositoryDaoImpl.java
@@ -0,0 +1,143 @@
+package com.smartitengineering.cms.repo.dao.impl;
+
+import com.google.inject.Inject;
+import com.smartitengineering.cms.api.content.Content;
+import com.smartitengineering.cms.api.content.ContentId;
+import com.smartitengineering.cms.api.factory.SmartContentAPI;
+import com.smartitengineering.cms.api.factory.content.WriteableContent;
+import com.smartitengineering.cms.api.workspace.WorkspaceId;
+import com.smartitengineering.dao.common.CommonDao;
+import com.smartitengineering.dao.common.queryparam.QueryParameter;
+import com.smartitengineering.util.bean.adapter.GenericAdapter;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class RepositoryDaoImpl<T extends AbstractRepositoryDomain<T>> implements CommonDao<T, String> {
+
+  @Inject
+  private WorkspaceId defaultContainerWorkspace;
+  @Inject
+  private GenericAdapter<Content, T> adapter;
+  protected transient final Logger logger = LoggerFactory.getLogger(getClass());
+
+  protected ContentId getContentId(String id) {
+    final byte[] bytesUtf8 = org.apache.commons.codec.binary.StringUtils.getBytesUtf8(id);
+    ContentId cId = SmartContentAPI.getInstance().getContentLoader().createContentId(defaultContainerWorkspace,
+                                                                                     bytesUtf8);
+    return cId;
+  }
+
+  public Set<T> getAll() {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public Set<T> getByIds(List<String> ids) {
+    Set<T> cs = new LinkedHashSet<T>();
+    for (String id : ids) {
+      T bean = getById(id);
+      if (bean != null) {
+        cs.add(bean);
+      }
+    }
+    return cs;
+  }
+
+  public T getById(String id) {
+    ContentId contentId = getContentId(id);
+    Content content = SmartContentAPI.getInstance().getContentLoader().loadContent(contentId);
+    if (content == null) {
+      return null;
+    }
+    else {
+      return adapter.convert(content);
+    }
+  }
+
+  public T getSingle(List<QueryParameter> query) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public List<T> getList(List<QueryParameter> query) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public <OtherTemplate> OtherTemplate getOther(List<QueryParameter> query) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public <OtherTemplate> List<OtherTemplate> getOtherList(List<QueryParameter> query) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public T getSingle(QueryParameter... query) {
+    return getSingle(Arrays.asList(query));
+  }
+
+  public List<T> getList(QueryParameter... query) {
+    return getList(Arrays.asList(query));
+  }
+
+  public <OtherTemplate> OtherTemplate getOther(QueryParameter... query) {
+    return getOther(Arrays.asList(query));
+  }
+
+  public <OtherTemplate> List<OtherTemplate> getOtherList(QueryParameter... query) {
+    return getOtherList(Arrays.asList(query));
+  }
+
+  public void save(T... states) {
+    for (T state : states) {
+      Content content = adapter.convertInversely(state);
+      if (content != null) {
+        WriteableContent wContent = SmartContentAPI.getInstance().getContentLoader().getWritableContent(content);
+        try {
+          wContent.put();
+        }
+        catch (Exception ex) {
+          logger.warn("Could not save content ", ex);
+          throw new RuntimeException(ex);
+        }
+      }
+    }
+  }
+
+  public void update(T... states) {
+    for (T state : states) {
+      Content content = adapter.convertInversely(state);
+      if (content != null) {
+        WriteableContent wContent = SmartContentAPI.getInstance().getContentLoader().getWritableContent(content);
+        try {
+          wContent.put();
+        }
+        catch (Exception ex) {
+          logger.warn("Could not save content ", ex);
+          throw new RuntimeException(ex);
+        }
+      }
+    }
+  }
+
+  public void delete(T... states) {
+    for (T state : states) {
+      Content content = adapter.convertInversely(state);
+      if (content != null) {
+        WriteableContent wContent = SmartContentAPI.getInstance().getContentLoader().getWritableContent(content);
+        try {
+          wContent.delete();
+        }
+        catch (Exception ex) {
+          logger.warn("Could not save content ", ex);
+          throw new RuntimeException(ex);
+        }
+      }
+    }
+  }
+}
