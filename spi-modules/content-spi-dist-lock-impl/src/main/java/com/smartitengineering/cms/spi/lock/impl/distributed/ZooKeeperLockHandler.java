309efacb9c53c7cd6f6e27608fff2a5dacb728a9
Imran M Yousuf <imyousuf@smartitengineering.com>
2011-11-29 19:14:39 +0600

    Establish connection to zookeeper server
    This involves being able to connect to the ZooKeeper and ensure that
    the expected root node exists.
    
    The test case starts up a ZooKeeper server, thanks to code written in
    HBase's MiniZooKeeperCluster and makes the proper checks.
    
    SLF4J is added as dependency for logging purpose.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- spi-modules/content-spi-dist-lock-impl/src/main/java/com/smartitengineering/cms/spi/lock/impl/distributed/ZooKeeperLockHandler.java
+++ spi-modules/content-spi-dist-lock-impl/src/main/java/com/smartitengineering/cms/spi/lock/impl/distributed/ZooKeeperLockHandler.java
@@ -18,15 +18,59 @@
  */
 package com.smartitengineering.cms.spi.lock.impl.distributed;
 
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import com.smartitengineering.cms.api.factory.write.Lock;
 import com.smartitengineering.cms.spi.lock.Key;
 import com.smartitengineering.cms.spi.lock.LockHandler;
+import java.io.IOException;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author imyousuf
  */
-public class ZooKeeperLockHandler implements LockHandler {
+public class ZooKeeperLockHandler implements LockHandler, Watcher {
+
+  private final ZooKeeper zooKeeper;
+  private final transient Logger logger = LoggerFactory.getLogger(getClass());
+  private final String rootNode;
+
+  @Inject
+  public ZooKeeperLockHandler(@Named("zkConnectString") final String connectString,
+                              @Named("zkRootNode") final String rootNode,
+                              @Named("zkTimeout") final int timeout) {
+    try {
+      zooKeeper = new ZooKeeper(connectString, timeout, this);
+    }
+    catch (IOException ex) {
+      logger.error("Could not intialize ZooKeeper connection!");
+      throw new IllegalStateException(ex);
+    }
+    if (rootNode.startsWith("/")) {
+      this.rootNode = rootNode;
+    }
+    else {
+      this.rootNode = new StringBuilder("/").append(rootNode).toString();
+    }
+    logger.info("Connected to ZooKeeper server");
+    try {
+      initializeRootNode();
+    }
+    catch (Exception ex) {
+      logger.error("Could not create root node!");
+      throw new IllegalStateException(ex);
+    }
+    logger.info("Created root node");
+  }
 
   public Lock register(Key key) {
     throw new UnsupportedOperationException("Not supported yet.");
@@ -34,5 +78,16 @@ public class ZooKeeperLockHandler implements LockHandler {
 
   public void unregister(Key key) {
     throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+  public void process(WatchedEvent event) {
+    //TODO Process WatchedEvent
+  }
+
+  private void initializeRootNode() throws KeeperException, InterruptedException {
+    final Stat stat = zooKeeper.exists(rootNode, false);
+    if (stat == null) {
+      zooKeeper.create(rootNode, new byte[]{}, Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+    }
   }
 }
--- spi-modules/content-spi-dist-lock-impl/src/test/java/com/smartitengineering/cms/spi/lock/impl/distributed/AppTest.java
+++ spi-modules/content-spi-dist-lock-impl/src/test/java/com/smartitengineering/cms/spi/lock/impl/distributed/AppTest.java
@@ -1,38 +1,143 @@
 package com.smartitengineering.cms.spi.lock.impl.distributed;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import junit.framework.Assert;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.server.NIOServerCnxn;
+import org.apache.zookeeper.server.ZooKeeperServer;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  public static final String ROOT_NODE = "/smart-cms";
+  private static NIOServerCnxn.Factory standaloneServerFactory;
+  private static final int CLIENT_PORT = 3882;
+  private static final int CONNECTION_TIMEOUT = 30000;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+  @BeforeClass
+  public static void startZooKeeperServer() {
+    try {
+      File snapDir = new File("./target/zk/");
+      snapDir.mkdirs();
+      ZooKeeperServer server = new ZooKeeperServer(snapDir, snapDir, 2000);
+      standaloneServerFactory = new NIOServerCnxn.Factory(new InetSocketAddress(CLIENT_PORT));
+      standaloneServerFactory.startup(server);
+      if (!waitForServerUp(CLIENT_PORT, CONNECTION_TIMEOUT)) {
+        throw new IOException("Waiting for startup of standalone server");
+      }
     }
+    catch (Exception ex) {
+      throw new IllegalStateException(ex);
+    }
+  }
+
+  @AfterClass
+  public static void shutdownZooKeeperServer() {
+    standaloneServerFactory.shutdown();
+    if (!waitForServerDown(CLIENT_PORT, CONNECTION_TIMEOUT)) {
+      throw new IllegalStateException("Waiting for shutdown of standalone server");
+    }
+  }
+
+  // XXX: From o.a.zk.t.ClientBase
+  private static boolean waitForServerUp(int port, long timeout) {
+    long start = System.currentTimeMillis();
+    while (true) {
+      try {
+        Socket sock = new Socket("localhost", port);
+        BufferedReader reader = null;
+        try {
+          OutputStream outstream = sock.getOutputStream();
+          outstream.write("stat".getBytes());
+          outstream.flush();
+
+          Reader isr = new InputStreamReader(sock.getInputStream());
+          reader = new BufferedReader(isr);
+          String line = reader.readLine();
+          if (line != null && line.startsWith("Zookeeper version:")) {
+            return true;
+          }
+        }
+        finally {
+          sock.close();
+          if (reader != null) {
+            reader.close();
+          }
+        }
+      }
+      catch (IOException e) {
+        // ignore as this is expected
+      }
+
+      if (System.currentTimeMillis() > start + timeout) {
+        break;
+      }
+      try {
+        Thread.sleep(250);
+      }
+      catch (InterruptedException e) {
+        // ignore
+      }
+    }
+    return false;
+  }
+
+  // XXX: From o.a.zk.t.ClientBase
+  private static boolean waitForServerDown(int port, long timeout) {
+    long start = System.currentTimeMillis();
+    while (true) {
+      try {
+        Socket sock = new Socket("localhost", CLIENT_PORT);
+        try {
+          OutputStream outstream = sock.getOutputStream();
+          outstream.write("stat".getBytes());
+          outstream.flush();
+        }
+        finally {
+          sock.close();
+        }
+      }
+      catch (IOException e) {
+        return true;
+      }
+
+      if (System.currentTimeMillis() > start + timeout) {
+        break;
+      }
+      try {
+        Thread.sleep(250);
+      }
+      catch (InterruptedException e) {
+        // ignore
+      }
+    }
+    return false;
+  }
+
+  @Test
+  public void testInitialization() throws Exception {
+    final String connectString = "localhost:" + CLIENT_PORT;
+    ZooKeeper zooKeeper = new ZooKeeper(connectString, CONNECTION_TIMEOUT, new Watcher() {
+
+      public void process(WatchedEvent event) {
+      }
+    });
+    Stat stat = zooKeeper.exists(ROOT_NODE, false);
+    Assert.assertNull(stat);
+    ZooKeeperLockHandler handler = new ZooKeeperLockHandler(connectString, ROOT_NODE, CONNECTION_TIMEOUT);
+    stat = zooKeeper.exists(ROOT_NODE, false);
+    Assert.assertNotNull(stat);
+  }
 }
