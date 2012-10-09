07348a9306c0760f181379c39d0c477130e75038
Imran M Yousuf <imyousuf@smartitengineering.com>
2012-10-10 02:00:34 +0600

    Add equals and hashcode for workspace impl
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- api-modules/content-api-impl/src/main/java/com/smartitengineering/cms/api/impl/workspace/WorkspaceImpl.java
+++ api-modules/content-api-impl/src/main/java/com/smartitengineering/cms/api/impl/workspace/WorkspaceImpl.java
@@ -81,4 +81,26 @@ public class WorkspaceImpl implements PersistableWorkspace {
   public Collection<ContentId> getRootContents() {
     return SmartContentSPI.getInstance().getWorkspaceService().getRootContents(id);
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final WorkspaceImpl other = (WorkspaceImpl) obj;
+    if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 3;
+    hash = 89 * hash + (this.id != null ? this.id.hashCode() : 0);
+    return hash;
+  }
 }
