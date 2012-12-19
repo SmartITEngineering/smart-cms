88d1cf41cce9f52576517a41d21d781aa2ffa5c2
Imran M Yousuf <imyousuf@smitsol.com>
2012-12-19 20:27:56 +0600

    The annotation that should be used for marking transaction execution
    This is a method level annotation aimed to mark the entry point of a
    transaction. Every transaction can set its isolation and propagation setting
    individually.
    
    This is starting and center point of transaction implementation.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smitsol.com>
    Issue #SMARTCMS-50 - Allow In-Memory transaction capabilities for Smart CMS DTOs

---
+++ code-generation/content-repo-dao-impl/src/main/java/com/smartitengineering/cms/repo/dao/impl/tx/Transactional.java
@@ -0,0 +1,31 @@
+package com.smartitengineering.cms.repo.dao.impl.tx;
+
+import com.google.inject.BindingAnnotation;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation for notifying that a method should start a transaction if not already started. Transaction Propagation
+ * @author imyousuf
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+@Documented
+@BindingAnnotation
+public @interface Transactional {
+
+  /**
+   * Configures this transaction to be an isolated from rest of the instance or whether it should not care of it.
+   * @return By default its true; i.e. isolated from other parallel transactions
+   */
+  boolean isolated() default true;
+
+  /**
+   * Configures whether the transaction requires a new transaction or can use the existing one
+   * @return By default its true; i.e. transaction will be propagated
+   */
+  boolean propagationRequired() default true;
+}
